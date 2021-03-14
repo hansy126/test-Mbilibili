43ac7aa9bfb182ddc51f221c9aa4ce602a021738
hansy126 <1300256413@qq.com>
2021-03-14 22:59:57 +0800

    实现nav区域功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,25 @@
 const vm = new Vue({
     el: '#app',
     data: {
-        j
+        navList: [],
+        navHide: true,
+        navActiveIndex : '',
     },
-    methods:{
-        
-    }
+
+    methods: {
+        handleClick() {
+            this.navHide = !this.navHide;
+        },
+        handleNavCliak(index) {
+            this.navActiveIndex = index
+        }
+    },
+    created() {
+        axios.get('https://developer.duyiedu.com/vue/bz/nav').then(res => {
+            console.log(res);
+            if (res.status === 200) {
+                this.navList = res.data.data;
+            }
+        })
+    },
 })
